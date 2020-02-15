1450c29f9fb24944221ec21980567e699afc2f45
Shubham Kumar Jha <skjha60070@gmail.com>
2020-02-15 23:12:35 +0530

    Added Past Results

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const api = {
   key: "14a785cecb277136e9727514a1ee0188",
   base: "https://api.openweathermap.org/data/2.5/",
-  basef: "https://api.weatherbit.io/v2.0/forecast/daily?city=",
-  keyf: "81aa86f589ee4a8798d0ca73b4c3411b",
-  keyp: "81aa86f589ee4a8798d0ca73b4c3411b",
-  basep: ""
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -21,10 +17,9 @@ function getResults (query) {
     .then(weather => {
       return weather.json();
     }).then(displayResults);
-  fetch(`${api.basef}${query},NC&key=${api.keyf}`)
+  fetch(`${api.base}forecast?q=${query}&appid=${api.key}`)
   .then(future => {
-      console.log("Fetching second api");
-      return future.json();
+    return future.json();
   }).then(displayFuture);
 }
 
@@ -48,18 +43,22 @@ function displayResults (weather) {
 }
 
 function displayFuture(future) {
-    console.log(future);
-    let avg = document.querySelector('.current .avg');
-    avg.innerHTML = `${Math.round(future.data[0].max_temp)-273}<span>°c</span>`;
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
 
-    let high = document.querySelector('.current .avg');
-    high.innerHTML = `${Math.round(future.data[0].max_temp)-271}<span>°c</span>`;
+  console.log(future);
+  let avg = document.querySelector('.current .avg');
+  avg.innerHTML = `${Math.round(future.list[0].main.temp)-273}<span>°c</span>`;
 
-    let low = document.querySelector('.current .low');
-    low.innerHTML = `${Math.round(future.data[0].min_temp)-273}<span>°c</span>`;
+  let low = document.querySelector('.current .low');
+  low.innerHTML = `${Math.round(future.list[0].main.temp_min)-273}<span>°c</span>`;
 
-    let speed = document.querySelector('.current .speed');
-    speed.innerHTML = `${Math.round(future.data[0].wind_spd)}<span>km/h</span>`;
+  let high = document.querySelector('.current .high');
+  high.innerHTML = `${Math.round(future.list[0].main.temp_max)-273}<span>°c</span>`;
+
+  let speed = document.querySelector('.current .speed');
+  speed.innerHTML = `${Math.round(future.list[0].wind.speed)}<span>km/h</span>`;
 }
 
 function dateBuilder (d) {
